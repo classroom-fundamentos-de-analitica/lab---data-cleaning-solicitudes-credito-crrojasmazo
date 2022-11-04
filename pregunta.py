bc2debf9b678aeec2de35cf03ea0602220bd7036
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-11-04 12:09:42 -0500

    commit 1
    fechas, nombres columna, na y duplicados

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,34 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import datetime
+import re
 
 def clean_data():
     
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    #leer dataframe
+    df = pd.read_csv("solicitudes_credito.csv", sep=";" )
 
-    #
-    # Inserte su código aquí
-    #
+    
+    
+    #Eliminar  NA y duplicados
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
+
+
+    #titulos y las columnas 
+    for column in df.columns:
+        if df[column].dtypes == object:
+            df[column] = df[column].str.lower()
+            df[column] = df[column].str.replace('-', ' ')
+            df[column] = df[column].str.replace('_', ' ')
+
+
+    #fecha en formato correcto
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(
+        lambda x: datetime.datetime.strptime(x, '%Y/%m/%d') 
+        if re.match('^\d{4}\/', x) 
+        else datetime.datetime.strptime(x, '%d/%m/%Y')
+    )
 
     return df
