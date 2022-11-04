66f6b90055d7e024588d4d344e9767090fa23750
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-11-04 12:26:11 -0500

    arreglo de duplicados
    valores de moneda listos

--- pregunta.py
+++ pregunta.py
@@ -13,15 +13,15 @@ import re
 def clean_data():
     
     #leer dataframe
-    df = pd.read_csv("solicitudes_credito.csv", sep=";" )
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",  index_col = 0)
 
     
     
     #Eliminar  NA y duplicados
     df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
+   
 
-
+    
     #titulos y las columnas 
     for column in df.columns:
         if df[column].dtypes == object:
@@ -29,6 +29,11 @@ def clean_data():
             df[column] = df[column].str.replace('-', ' ')
             df[column] = df[column].str.replace('_', ' ')
 
+    #montos enteros
+    df.monto_del_credito = df.monto_del_credito.str.strip("$")
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
 
     #fecha en formato correcto
     df.fecha_de_beneficio = df.fecha_de_beneficio.apply(
@@ -36,5 +41,8 @@ def clean_data():
         if re.match('^\d{4}\/', x) 
         else datetime.datetime.strptime(x, '%d/%m/%Y')
     )
+    df.drop_duplicates(inplace = True)
+    
 
     return df
+
